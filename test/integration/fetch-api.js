c17535e33adec3ef2ad30407c547a6555952f1ae
Evert Pot <me@evertpot.com>
2017-08-03 11:26:47 -0400

    Making it easier to fire off custom requests with the Fetch API.

--- test/integration/fetch-api.js
+++ test/integration/fetch-api.js
@@ -6,13 +6,14 @@ const Request = require('node-fetch').Request;
 describe('Using the fetch api', () => {
 
   let hal2;
+  let client;
   before( async () => {
-    const client = new Client('http://localhost:3000/hal1.json');
+    client = new Client('http://localhost:3000/hal1.json');
     hal2 = await client.follow('next');
   });
 
   it('should return a response object', async() => {
-  
+
     const response = await hal2.fetch('?foo=bar');
     expect(response).to.have.property('status');
     expect(response.status).to.eql(200);
@@ -20,11 +21,25 @@ describe('Using the fetch api', () => {
   });
 
   it('should also work when passing a Request object', async() => {
- 
-    const request = new Request('?foo=bar'); 
+
+    const request = new Request('?foo=bar');
     const response = await hal2.fetch(request);
     expect(response).to.have.property('status');
     expect(response.status).to.eql(200);
+
+  });
+
+  it('should allow overriding the HTTP method', async() => {
+
+    const response = await hal2.fetch({ method: 'PUT' });
+    expect(response).to.have.property('status');
+    expect(response.status).to.eql(204);
+
+  });
+  after( async() => {
+
+    // Clearing any changes.
+    await client.getResource('/reset').post({});
 
   });
 
