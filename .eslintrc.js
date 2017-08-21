01e98bad92a63563aece63c8a2a249360a5d8ff9
Evert Pot <me@evertpot.com>
2017-08-20 20:47:00 -0400

    Warning on invalid jsdoc blocks.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "node": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2 
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  "env": {
+      "browser": true,
+      "commonjs": true,
+      "node": true,
+      "es6": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "valid-jsdoc" : [
+      "warn",
+      {
+        "requireReturn" : false,
+        "requireParamDescription" : false,
+        "requireReturnDescription" : false
+      }
+    ]
+  }
 };
--- Makefile
+++ Makefile
@@ -9,6 +9,10 @@ clean:
 test:
 	npm test
 
+.PHONY: lint
+lint:
+	node_modules/.bin/eslint lib/
+
 dist/restl.js: lib/*.js
 	mkdir -p dist
 	node_modules/.bin/webpack \
