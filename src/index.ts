acaf5987528a5e8ea01cf156399434d7443dd4df
Evert Pot <me@evertpot.com>
2018-08-23 17:35:23 +0200

    Exporting resource

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-import Ketting from './ketting';
-export default Ketting;
+export { default } from './ketting';
+export { default as Ketting } from './ketting';
+export { default as Resource } from './resource';
