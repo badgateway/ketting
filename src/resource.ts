33fad74ece7fcd31d161ad832252e9e7f88fc477
Evert Pot <me@evertpot.com>
2018-10-20 19:17:05 -0400

    Generic Resource

--- src/resource.ts
+++ src/resource.ts
@@ -20,7 +20,7 @@ import { resolve } from './utils/url';
  * @param {string} uri
  * @constructor
  */
-export default class Resource<T = any> {
+export default class Resource<T> {
 
   /**
    * Reference to the main Client
