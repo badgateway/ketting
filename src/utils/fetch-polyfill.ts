a373ca2be3d14e72e0d4cf5404aa2f5f0bf305b4
Evert Pot <me@evertpot.com>
2019-04-18 23:02:17 -0400

    No longer ship a fetch polyfill

--- src/utils/fetch-polyfill.ts
+++
@@ -1,6 +0,0 @@
-import { default as nodeFetch, Headers, Request } from 'node-fetch';
-
-// Registering Fetch as a glboal polyfill
-(<any> global).fetch = nodeFetch;
-(<any> global).Request = Request;
-(<any> global).Headers = Headers;
