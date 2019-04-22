a32d0827edb464972380b06a057d59e7cd4c5b57
Evert Pot <me@evertpot.com>
2019-04-22 12:32:49 -0400

    No longer need 'createFetchRequest'

--- src/utils/fetch-helper.ts
+++ src/utils/fetch-helper.ts
@@ -109,37 +109,6 @@ export default class FetchHelper {
 
 }
 
-/**
- * Creates a Fetch Request object, based on a number of settings.
- *
- * @param {string|Request} input - Url or input request object.
- * @param {object} init - A list of Fetch settings
- * @param {object} defaultInit - A list of default settings to use if they
- *                              weren't overridden by init.
- * @return {Response}
- */
-export function createFetchRequest(input: any, init: any, defaultInit: any): Request {
-
-  const trueInit: any = {};
-
-  if (init) {
-    Object.assign(trueInit, defaultInit, init);
-  } else {
-    Object.assign(trueInit, defaultInit);
-  }
-
-  trueInit.headers = mergeHeaders([
-    defaultInit ? defaultInit.headers : null,
-    // @ts-ignore cross-fetch definitions are broken. See https://github.com/lquixada/cross-fetch/pull/19
-    input instanceof Request ? input.headers : null,
-    init && init.headers ? init.headers : null
-  ]);
-
-    // @ts-ignore cross-fetch definitions are broken. See https://github.com/lquixada/cross-fetch/pull/19
-  return new Request(input, trueInit);
-
-}
-
 type HeaderSet = any;
 
 
