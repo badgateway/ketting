323d6eeb52545e8f7e515b4f66e20fe93c46f990
Evert Pot <me@evertpot.com>
2019-06-16 16:04:48 -0400

    Fix the default export for browser distros and nodejs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export { default as default } from './ketting';
-export { default as Ketting } from './ketting';
-export { default as Resource } from './resource';
+import BaseKetting from './ketting';
+import Resource from './resource';
+
+class Ketting extends BaseKetting {
+
+  static Resource = Resource;
+
+}
+
+module.exports = Ketting;
