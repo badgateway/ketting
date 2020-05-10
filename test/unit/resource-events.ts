bad1e2f427299c8d4e186726f3c5f9450b33512d
Evert Pot <me@evertpot.com>
2020-05-10 01:50:23 -0400

    Testing the 'stale' event.

--- test/unit/resource-events.ts
+++ test/unit/resource-events.ts
@@ -8,11 +8,8 @@ describe('Resource Events', () => {
     it('should trigger when an unsafe method is used', async () => {
 
       const client = new Client('http://example');
-      client.use( async(req, next): Promise<Response> => {
+      client.use( mockFetchMw );
 
-        return new Response('OK!');
-
-      });
       const resource = client.go('/res');
       await resource.get();
 
@@ -27,11 +24,8 @@ describe('Resource Events', () => {
     it('should trigger when an unsafe method is used via fetch', async () => {
 
       const client = new Client('http://example');
-      client.use( async(req, next): Promise<Response> => {
+      client.use( mockFetchMw );
 
-        return new Response('OK!');
-
-      });
       const resource = client.go('/res');
       await resource.get();
 
@@ -46,11 +40,7 @@ describe('Resource Events', () => {
     it('should not trigger when a safe method is used via fetch', async () => {
 
       const client = new Client('http://example');
-      client.use( async(req, next): Promise<Response> => {
-
-        return new Response('OK!');
-
-      });
+      client.use( mockFetchMw );
       const resource = client.go('/res');
       await resource.get();
 
@@ -65,11 +55,7 @@ describe('Resource Events', () => {
     it('should trigger when an unsafe method is used via the global fetcher', async () => {
 
       const client = new Client('http://example');
-      client.use( async(req, next): Promise<Response> => {
-
-        return new Response('OK!');
-
-      });
+      client.use( mockFetchMw );
       const resource = client.go('/res');
       await resource.get();
 
@@ -84,11 +70,7 @@ describe('Resource Events', () => {
     it('should not trigger when a safe method is used via the global fetcher', async () => {
 
       const client = new Client('http://example');
-      client.use( async(req, next): Promise<Response> => {
-
-        return new Response('OK!');
-
-      });
+      client.use( mockFetchMw );
       const resource = client.go('/res');
       await resource.get();
 
@@ -100,7 +82,84 @@ describe('Resource Events', () => {
       expect(triggered).to.equal(false);
 
     });
+    it('should not trigger for an unrelated request', async() => {
+
+      const client = new Client('http://example');
+      client.use( mockFetchMw );
+      const resource = client.go('/res');
+      await resource.get();
+
+      let triggered = false;
+      resource.once('stale', () => {
+        triggered = true;
+      });
+      await client.fetcher.fetch('http://example/unrelated', { method: 'POST' });
+      expect(triggered).to.equal(false);
+
+    });
+    it('should trigger when a response contains a relevant Location header', async() => {
+
+      const client = new Client('http://example');
+      client.use( mockFetchMw );
+      const resource = client.go('/res');
+      await resource.get();
+
+      let triggered = false;
+      resource.once('stale', () => {
+        triggered = true;
+      });
+      await client.fetcher.fetch('http://example/location', { method: 'POST' });
+      expect(triggered).to.equal(true);
+
+    });
+    it('should trigger when a response contains a relevant Content-Location header', async() => {
+
+      const client = new Client('http://example');
+      client.use( mockFetchMw );
+      const resource = client.go('/res');
+      await resource.get();
+
+      let triggered = false;
+      resource.once('stale', () => {
+        triggered = true;
+      });
+      await client.fetcher.fetch('http://example/content-location', { method: 'POST' });
+      expect(triggered).to.equal(true);
+
+    });
+    it('should trigger when a response contains a relevant Link header', async() => {
+
+      const client = new Client('http://example');
+      client.use( mockFetchMw );
+      const resource = client.go('/res');
+      await resource.get();
+
+      let triggered = false;
+      resource.once('stale', () => {
+        triggered = true;
+      });
+      await client.fetcher.fetch('http://example/link', { method: 'POST' });
+      expect(triggered).to.equal(true);
+
+    });
 
   });
 
 });
+
+const mockFetchMw = async(req: Request): Promise<Response> => {
+  const headers = new Headers();
+  switch(req.url) {
+    case 'http://example/location' :
+      headers.set('Location', '/res');
+      break;
+    case 'http://example/content-location' :
+      headers.set('Content-Location', '/res');
+      break;
+    case 'http://example/link' :
+      headers.set('Link', '</res>; rel=invalidates');
+      break;
+  }
+
+  return new Response('OK!', { headers });
+}
