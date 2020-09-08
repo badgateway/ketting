b1672d6f9a5e004cf57796f482ac611fd3c90d95
Evert Pot <me@evertpot.com>
2020-09-08 01:00:12 -0400

    Harder linting fixes

--- src/field.ts
+++ src/field.ts
@@ -6,34 +6,26 @@ export interface BaseField<TType extends string, TValue> {
   required: boolean;
   readOnly: boolean;
   label?: string;
-};
-
-export interface Checkbox extends BaseField<'checkbox', boolean> {
 }
 
-export interface Color extends BaseField<'color', string> {
-}
+export type Checkbox = BaseField<'checkbox', boolean>
 
-export interface Date extends BaseField<'date', string> {
-}
+export type Color = BaseField<'color', string>
+
+export type Date = BaseField<'date', string>
 
 /**
  * @deprecated
  */
-export interface DateTime extends BaseField<'datetime', Date> {
-}
+export type DateTime = BaseField<'datetime', Date>
 
-export interface DateTimeLocal extends BaseField<'datetime-local', Date> {
-}
+export type DateTimeLocal = BaseField<'datetime-local', Date>
 
-export interface Email extends BaseField<'email', string> {
-}
+export type Email = BaseField<'email', string>
 
-export interface File extends BaseField<'file', never> {
-}
+export type File = BaseField<'file', never>
 
-export interface Hidden extends BaseField<'hidden', string> {
-}
+export type Hidden = BaseField<'hidden', string>
 
 export interface Number extends BaseField<'number', number> {
   max?: number;
@@ -41,11 +33,9 @@ export interface Number extends BaseField<'number', number> {
   step?: number;
 }
 
-export interface Month extends BaseField<'month', string> {
-}
+export type Month = BaseField<'month', string>
 
-export interface Password extends BaseField<'password', string> {
-}
+export type Password = BaseField<'password', string>
 
 export interface Radio extends BaseField<'radio', string> {
   options?: Map<string, string>;
@@ -57,11 +47,9 @@ export interface Range extends BaseField<'range', number> {
   step?: number;
 }
 
-export interface Search extends BaseField<'search', string> {
-}
+export type Search = BaseField<'search', string>
 
-export interface Tel extends BaseField<'tel', string> {
-}
+export type Tel = BaseField<'tel', string>
 
 export interface Text extends BaseField<'text', string> {
   minLength?: number;
@@ -70,15 +58,14 @@ export interface Text extends BaseField<'text', string> {
   options?: Map<string, string>;
 }
 
-export interface Time extends BaseField<'time', string> {
-}
+export type Time = BaseField<'time', string>
 
-export interface Url extends BaseField<'url', string> {
-}
+export type Url = BaseField<'url', string>
 
-export interface Week extends BaseField<'week', string> {
-}
+export type Week = BaseField<'week', string>
 
+// eslint will want to fix the number type here
 export type Field = Checkbox | Color | Date | DateTime | DateTimeLocal | Email
+// eslint-disable-next-line
   | File | Hidden | Number | Month | Password | Radio | Range | Search | Tel
   | Text | Time | Url | Week; 
