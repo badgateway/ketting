40498bfd3da2ce50d3913da41b4bed38d156c282
Evert Pot <me@evertpot.com>
2020-12-19 16:09:50 -0500

    Refactor middlewares into their own directory.
    Reduces the bulkiness a bit, especially since we're adding more.

---
+++ src/middlewares/accept-header.ts
@@ -0,0 +1,24 @@
+import { FetchMiddleware } from '../http/fetcher';
+import Client from '../client';
+
+/**
+ * This middleware injects a default Accept header.
+ *
+ * The list of content-types is generated from the Client's
+ * 'contentTypeMap'.
+ */
+export default function(client: Client): FetchMiddleware {
+
+  return async(request, next) => {
+
+    if (!request.headers.has('Accept')) {
+      const acceptHeader = Object.entries(client.contentTypeMap).map(
+        ([contentType, [stateFactory, q]]) => contentType + ';q=' + q
+      ).join(', ');
+      request.headers.set('Accept', acceptHeader);
+    }
+    return next(request);
+
+  };
+
+};
