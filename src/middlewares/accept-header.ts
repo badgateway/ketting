ac57f4a99fb0c0dd9e85d70b7b041750a90358b6
Evert Pot <me@evertpot.com>
2020-12-19 16:18:06 -0500

    Lint

--- src/middlewares/accept-header.ts
+++ src/middlewares/accept-header.ts
@@ -21,4 +21,4 @@ export default function(client: Client): FetchMiddleware {
 
   };
 
-};
+}
--- src/middlewares/cache-expire.ts
+++ src/middlewares/cache-expire.ts
@@ -84,3 +84,5 @@ export default function(client: Client): FetchMiddleware {
     return response;
 
   };
+
+}
