d46945ffca7a895f5034327f603148bb9dcce7be
hthomas <hthomas@cosium.com>
2021-08-23 11:41:55 +0200

    Pull-request review's correction

--- test/integration/resource.ts
+++ test/integration/resource.ts
@@ -4,38 +4,38 @@ import {Ketting, NeverCache} from '../../src';
 describe('Using resources', async () => {
   const ketting = new Ketting('http://localhost:3000/hal1.json');
 
-  it('concurrent get on the same resource should do 2 distinct HTTP calls if request headers are different', async () => {
+  it('should do 2 distinct HTTP calls if request headers are different on concurrent calls', async () => {
     ketting.cache = new NeverCache();
 
     const resource = await ketting.follow('collection');
 
     const [firstState, secondState] =
       await Promise.all([
-        resource.get({headers: {'delay-in-ms': '2', 'foo': 'bar'}}),
-        resource.get({headers: {'foo': 'baz'}})
+        resource.get({headers: {'prefer': 'bar'}}),
+        resource.get({headers: {'prefer': 'baz'}})
       ]);
 
-    expect(firstState.timestamp).to.not.eql(secondState.timestamp);
+    expect(firstState.headers.get('preference-applied')).to.not.eql(secondState.headers.get('preference-applied'));
   });
 
-  it('concurrent get on the same resource should produce only one HTTP call if request headers are the same', async () => {
+  it('should produce only one HTTP call if request headers are the same on concurrent calls', async () => {
     ketting.cache = new NeverCache();
 
     const resource = await ketting.follow('collection');
 
-    const headers = new Headers();
-    headers.append('foo', 'bar');
-    headers.append('foo', 'baz');
+    const headers1 = new Headers();
+    headers1.append('prefer', 'bar');
+    headers1.append('prefer', 'baz');
     const headers2 = new Headers();
-    headers2.append('foo', 'bar');
-    headers2.append('foo', 'baz');
+    headers2.append('prefer', 'bar');
+    headers2.append('prefer', 'baz');
 
     const [firstState, secondState] =
       await Promise.all([
-        resource.get({headers: headers}),
+        resource.get({headers: headers1}),
         resource.get({headers: headers2})
       ]);
 
-    expect(firstState.timestamp).to.eql(secondState.timestamp);
+    expect(firstState.headers.get('response-id')).to.eql(secondState.headers.get('response-id'));
   });
 });
