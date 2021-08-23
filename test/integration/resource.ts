a8c34d2aadaab16caf89a3dfcfe27388e5f65cc1
hthomas <hthomas@cosium.com>
2021-08-23 15:03:47 +0200

    Pull-request review's correction

--- test/integration/resource.ts
+++ test/integration/resource.ts
@@ -1,7 +1,7 @@
 import {expect} from 'chai';
 import {Ketting, NeverCache} from '../../src';
 
-describe('Using resources', async () => {
+describe('Resource', async () => {
   const ketting = new Ketting('http://localhost:3000/hal1.json');
 
   it('should do 2 distinct HTTP calls if request headers are different on concurrent calls', async () => {
@@ -11,11 +11,11 @@ describe('Using resources', async () => {
 
     const [firstState, secondState] =
       await Promise.all([
-        resource.get({headers: {'delay-in-ms': '2', 'prefer': 'bar'}}),
-        resource.get({headers: {'delay-in-ms': '2', 'prefer': 'baz'}})
+        resource.get({headers: {'delay-in-ms': '2', 'foo': 'bar'}}),
+        resource.get({headers: {'delay-in-ms': '2', 'foo': 'baz'}})
       ]);
 
-    expect(firstState.headers.get('preference-applied')).to.not.eql(secondState.headers.get('preference-applied'));
+    expect(firstState.headers.get('response-id')).to.not.eql(secondState.headers.get('response-id'));
   });
 
   it('should produce only one HTTP call if request headers are the same on concurrent calls', async () => {
@@ -24,12 +24,12 @@ describe('Using resources', async () => {
     const resource = await ketting.follow('collection');
 
     const headers1 = new Headers();
-    headers1.append('prefer', 'bar');
-    headers1.append('prefer', 'baz');
+    headers1.append('foo', 'bar');
+    headers1.append('foo', 'baz');
     headers1.append('delay-in-ms', '2');
     const headers2 = new Headers();
-    headers2.append('prefer', 'bar');
-    headers2.append('prefer', 'baz');
+    headers2.append('foo', 'bar');
+    headers2.append('foo', 'baz');
     headers2.append('delay-in-ms', '2');
 
     const [firstState, secondState] =
