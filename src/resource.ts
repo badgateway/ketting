af45a7335f5c54fe7041f09faa3b241679404630
hthomas <hthomas@cosium.com>
2021-10-29 11:09:20 +0200

    Pull-request review's correction

--- src/resource.ts
+++ src/resource.ts
@@ -56,7 +56,7 @@ export class Resource<T = any> extends EventEmitter {
     const params = optionsToRequestInit('GET', getOptions);
     const uri = this.uri;
     if (!this.activeRefreshes.has(uri, getOptions)) {
-      this.activeRefreshes.put(uri, getOptions, (async (): Promise<State<T>> => {
+      this.activeRefreshes.set(uri, getOptions, (async (): Promise<State<T>> => {
         try {
           const response = await this.fetchOrThrow(params);
           const state = await this.client.getStateForResponse(uri, response);
@@ -106,7 +106,7 @@ export class Resource<T = any> extends EventEmitter {
     params.cache = 'no-cache';
     const uri = this.uri;
     if (!this.activeRefreshes.has(uri, getOptions)) {
-      this.activeRefreshes.put(uri, getOptions, (async (): Promise<State<T>> => {
+      this.activeRefreshes.set(uri, getOptions, (async (): Promise<State<T>> => {
         try {
           const response = await this.fetchOrThrow(params);
           const state = await this.client.getStateForResponse(uri, response);
@@ -510,7 +510,7 @@ function optionsToRequestInit(method: string, options?: GetRequestOptions | Post
 class ActiveRefreshes<T = any> {
   private readonly refreshByHash: Map<string, Promise<State<T>>> = new Map<string, Promise<State<T>>>();
 
-  put(uri: string, options: GetRequestOptions | undefined, activeRefresh: Promise<State<T>>): void {
+  set(uri: string, options: GetRequestOptions | undefined, activeRefresh: Promise<State<T>>): void {
     this.refreshByHash.set(this.hash(uri, options), activeRefresh);
   }
 
