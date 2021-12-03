db2b1398f1be525be03e4ba058bd6a5c1ba8cae3
Evert Pot <me@evertpot.com>
2021-12-03 02:19:25 -0500

    Fix lint errors

--- src/client.ts
+++ src/client.ts
@@ -40,14 +40,14 @@ export default class Client {
   contentTypeMap: {
     [mimeType: string]: [StateFactory<any>, string];
   } = {
-    'application/prs.hal-forms+json': [halStateFactory, '1.0'],
-    'application/hal+json': [halStateFactory, '0.9'],
-    'application/vnd.api+json': [jsonApiStateFactory, '0.8'],
-    'application/vnd.siren+json': [sirenStateFactory, '0.8'],
-    'application/vnd.collection+json': [cjStateFactory, '0.8'],
-    'application/json': [halStateFactory, '0.7'],
-    'text/html': [htmlStateFactory, '0.6'],
-  }
+      'application/prs.hal-forms+json': [halStateFactory, '1.0'],
+      'application/hal+json': [halStateFactory, '0.9'],
+      'application/vnd.api+json': [jsonApiStateFactory, '0.8'],
+      'application/vnd.siren+json': [sirenStateFactory, '0.8'],
+      'application/vnd.collection+json': [cjStateFactory, '0.8'],
+      'application/json': [halStateFactory, '0.7'],
+      'text/html': [htmlStateFactory, '0.6'],
+    };
 
   /**
    * The cache for 'State' objects
